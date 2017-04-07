e9260902f4b5e4d8f3d56d517cf9f454a8984927
YuxuanLing <yuxuan_ling@163.com>
2017-04-07 09:50:59 -0400

    modify

--- trunk/code/study/python/image_check/check_img.py
+++ trunk/code/study/python/image_check/check_img.py
@@ -11,9 +11,9 @@ class Nude(object):
 
 
     def __init__(self, path_or_image):
-        if isinstnce(path_or_image, Image.Image):
+        if isinstne(path_or_image, Image.Image):
             self.image = path_or_image
-        elif isinstance(path_or_omage,str):
+        elif isinstane(path_or_omage,str):
             self.image = Image.open(path_or_image)
 
 
@@ -33,6 +33,44 @@ class Nude(object):
     self.skin_map=[]
     self.detected_regions=[]
     self.merge_regions=[]
+    self.skin_regions=[]
+    self.last_from, self.last_to = -1, -1
+    self.result = None
+    self.mesage = None
+    self.width, self.height = self.image.size
+    self.total_pixels=self.width * self.height
 
 
+def resize(self, maxwidth=1000,maxheight=1000):
+    """
+    基于最大宽高按比例重设图片大小
+    """
+    ret = 0
+    if maxwidth:
+        if self.width > maxwidth:
+            wpercent = (maxwidth/self.width)
+            hsize    = int((self.height*wpercent))
+            fname = self.image.filename
+            #LANCZOS 是重采样滤波器，用于抗锯齿
+            self.image = self.image.resize((maxwidth,hsize), image.LANCZOS)
+            self.image.filename = fname
+            self.width, self.height = self.image.size
+            self.total_pixels = self.width * self.height
+            ret += 1
+
+    if maxheight:
+        if self.height > maxheight:
+            hpercent = (maxheight/self.height)
+            wsize    = int((self.width*hpercent))
+            fname = self.image.filename
+            #LANCZOS 是重采样滤波器，用于抗锯齿
+            self.image = self.image.resize((wsize,maxheight), image.LANCZOS)
+            self.image.filename = fname
+            self.width, self.height = self.image.size
+            self.total_pixels = self.width * self.height
+            ret += 2
+
+    return ret
+
+def parse(self):
 
