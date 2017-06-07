0720f0b4217b9e05fe43c377eb6638bd8f140190
yxling <yuxuan_ling@163.com>
2017-06-07 16:06:33 +0800

    fix bug of steganography.py

--- trunk/code/study/python/steganography.py
+++ trunk/code/study/python/steganography.py
@@ -6,7 +6,7 @@ def encodeDataInImg(image,data):
     binary = ''.join(map(constLenBin,bytearray(data,'utf-8')))
     if len(binary) > len(image.getdata())*4:
         raise Exception("Error: Can't encode more than " + len(evenImage.getdata())*4 + "bits in this image.")
-    encodedPixels = [(r+int(binary[index*4+0]),g+int(binary[index*4+1]),b+int(binary[index*4+2]),t+int(binary[index*4+3])) if index*4 < len(binary) else (r,g,b,t) for index,(r,g,b,t) in enumerate(list(evenImage.getdata()))]
+    encodedPixels = [(r+int(binary[index*4+0]),g+int(binary[index*4+1]),b+int(binary[index*4+2]),t+int(binary[index*4+3])) if index*4 < len(binary) else (r,g,b,t) for index,(r,g,b,t) in enumerate(list(evenImg.getdata()))]
     encodedImage = Image.new(evenImg.mode,evenImg.size)
     encodedImage.putdata(encodedPixels)
     return encodedImage
@@ -26,7 +26,7 @@ def decodeImage(image):
     pixels = list(image.getdata())  # 获得像素列表
     binary = ''.join([str(int(r>>1<<1!=r))+str(int(g>>1<<1!=g))+str(int(b>>1<<1!=b))+str(int(t>>1<<1!=t)) for (r,g,b,t) in pixels])  # 提取图片中所有最低有效位中的数据
     # 找到数据截止处的索引
-    locationDoubleNull = binary.find('0000000000000000')
+    locationDoubleNull = binary.find('0000000000000000')               ##为何截止符号是这个？？
     endIndex = locationDoubleNull+(8-(locationDoubleNull % 8)) if locationDoubleNull%8 != 0 else locationDoubleNull
     data = binaryToString(binary[0:endIndex])
     return data
@@ -36,7 +36,7 @@ def binaryToString(binary):
     string = []
     rec = lambda x, i: x[2:8] + (rec(x[8:], i-1) if i > 1 else '') if x else ''
     # rec = lambda x, i: x and (x[2:8] + (i > 1 and rec(x[8:], i-1) or '')) or ''
-    fun = lambda x, i: x[i+1:8] + rec(x[8:], i-1)
+    fun = lambda x, i: x[1:8] if i == 1 else x[i+1:8] + rec(x[8:], i-1)   ##取得编码的xxxx数字部分
     while index + 1 < len(binary):
         chartype = binary[index:].index('0')
         length = chartype*8 if chartype else 8
@@ -44,5 +44,5 @@ def binaryToString(binary):
         index += length
     return ''.join(string)
 
-encodeDataInImage(Image.open("coffee.png"), '你好世界，Hello world!').save('encodeImage.png')
+encodeDataInImg(Image.open("coffee.png"), '你好世界，Hello world!').save('encodeImage.png')
 print(decodeImage(Image.open("encodeImage.png")))    
