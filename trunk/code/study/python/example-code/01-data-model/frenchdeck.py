a934bb1c2c3d08b3455bf4069dd9b3fe6dfdd525
yxling <yuxuan_ling@163.com>
2017-06-15 09:47:51 +0800

    add gtest solution fro x265

--- trunk/code/study/python/example-code/01-data-model/frenchdeck.py
+++ trunk/code/study/python/example-code/01-data-model/frenchdeck.py
@@ -1,6 +1,6 @@
 import collections
 from random import choice
-
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
@@ -17,10 +17,17 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
+def spades_higt(card):
+    rank_value=FrenchDeck.ranks.index(card.rank)
+    return rank_value*len(suit_values) + suit_values[card.suit]
+
 
 if __name__=="__main__":
-    beer_card=Card('7','diamonds')
+    beer_card=Card('7','hearts')
     #print(beer_card)
     deck=FrenchDeck()
     for n, card in enumerate(deck, 1):
         print(n, card)
+    print("----------------------------------------------")
+    for card in sorted(deck, key=spades_higt):
+        print card
