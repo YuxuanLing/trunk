00c93d7e5b38673abb5a83bc9e8d3bfe762d9566
YuxuanLing <yuxuan_ling@163.com>
2018-03-07 17:41:02 +0800

    add learn code

---
+++ trunk/code/study/advanced_c++_learn/Parsing.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+   string filename = "stats.txt";
+   ifstream input;
+
+   input.open(filename.c_str());
+
+   if(!input.is_open()) {
+   		return 1; 
+   }
+
+	while(input) {
+		string line;
+		
+		
+		getline(input, line,':');
+
+		int population;
+		input >> population;
+
+		//input.get();
+        input >> ws;
+		if(!input) {
+			break;
+		}
+
+		cout << line << "--" << population<<endl;
+		
+	}
+
+	return 0;
+}
