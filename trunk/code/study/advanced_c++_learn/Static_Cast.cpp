1c305802168980c64c5d3341c7d8db2f28ce4cd9
YuxuanLing <yuxuan_ling@163.com>
2018-04-19 17:45:40 +0800

    add code

---
+++ trunk/code/study/advanced_c++_learn/Static_Cast.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+
+
+class Parent {
+
+};
+
+class Brother: public Parent {
+
+
+};
+
+
+class Sister: public Parent {
+
+};
+
+int main() {
+
+	Parent parent;
+	Brother brother;
+
+
+	return 0;
+
+
+}
