9166caa0d4ffcca141bed837c59e1aec43efbf4a
YuxuanLing <yuxuan_ling@163.com>
2018-04-25 17:16:31 +0800

    add code

--- trunk/code/study/advanced_c++_learn/Drawing_Fractal_Img/main.cpp
+++ trunk/code/study/advanced_c++_learn/Drawing_Fractal_Img/main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<cstdint>
+#include<memory>
+#include"Mandelbrot.h"
 #include "Bitmap.h"
 using namespace std;
 using namespace fractalofprogramming;
@@ -6,16 +9,41 @@ int main()
 {
     int const WIDTH = 800;
     int const HEIGHT = 600;
+	double min = 999999;
+	double max = -999999;
 	Bitmap bitmap(WIDTH,HEIGHT);
 
+
+	unique_ptr<int[]> histogram(new int[Mandelbrot::MAX_ITERATIONS + 1]{0});
 	bitmap.setPixel(WIDTH/2, HEIGHT/2, 255, 255, 255);
 
 	for(int y = 0; y < HEIGHT; y++)
 		for(int x = 0; x < WIDTH; x++) {
-		  bitmap.setPixel(x, y, 255, 0 , 0);
+		    double xFractal = (x - WIDTH/2 - 200) * 2.0/HEIGHT;
+		    double yFractal = (y - HEIGHT/2) * 2.0/HEIGHT;
+            
+			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
+            uint8_t color =
+				(uint8_t)(256*(double)iterations/Mandelbrot::MAX_ITERATIONS);
+
+            histogram[iterations]++;
+            color *=color*color; 
+
+			bitmap.setPixel(x, y, 0, color, 0);
+			if(color < min) min = color;
+			if(color > max) max = color;
 		}
 
+    int count = 0;
+    for(int i = 0; i <= Mandelbrot::MAX_ITERATIONS; i++) {
+	
+	    cout << histogram[i] << " " << flush;
+		count += histogram[i];
+	}
+    cout << endl;
+	cout << count << ";  " << WIDTH*HEIGHT << endl;
 
+    cout<< min << "," << max << endl;
 	bitmap.write("test.bmp");
 	
 	cout << "Hello World !" << endl;
